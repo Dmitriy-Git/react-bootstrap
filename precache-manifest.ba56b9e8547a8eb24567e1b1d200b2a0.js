5e4802b72345f405b8a66678fc5dfa51a0d47c88
Dmitriy Popko <popkodmitrii@yandex.ru>
2020-06-20 17:55:53 +0300

    Updates

---
+++ precache-manifest.ba56b9e8547a8eb24567e1b1d200b2a0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d336cdea834386e647045c5276cd14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0856e7fd43188f09edbc",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b149e6d95787c7396ed0",
+    "url": "/static/css/main.01102b2d.chunk.css"
+  },
+  {
+    "revision": "0856e7fd43188f09edbc",
+    "url": "/static/js/2.bab8787a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bab8787a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b149e6d95787c7396ed0",
+    "url": "/static/js/main.f3514918.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "b4330ef3923e7e2fbf76d598bc76d9e4",
+    "url": "/static/media/full-moon-in-blue-sky-4321944.b4330ef3.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d1f3937c9ea8e8dbaf87ef3811da772d",
+    "url": "/static/media/red-art-relaxation-girl-20967.d1f3937c.jpg"
+  }
+]);
